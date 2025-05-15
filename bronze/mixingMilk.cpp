cb458cb55e6866bf6f3b4fc831695a9e37a63de0
lightsteps <93688603+1ightstep@users.noreply.github.com>
2025-05-15 08:45:03 -0700

    mixingMilk.cpp

---
+++ bronze/mixingMilk.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    freopen("shell.in", "r", stdin);
+    
+    vector<int> capacity(3);
+    vector<int> milk(3);
+    for (int i = 0; i < 3; i++) 
+        scanf("%d %d", &capacity[i], &milk[i]);
+    
+    int a, b, amt;
+    for (int i = 0; i < 100; i++) {
+        a = i % 3; 
+        b = (i+1) % 3;
+        amt = min({milk[a], capacity[b]});
+        
+        milk[a] -= amt; 
+        milk[b] += amt;
+    }
+    
+    freopen("shell.out", "w", stdout);
+    for (int i : milk) 
+        printf("%d\n", i);
+}
